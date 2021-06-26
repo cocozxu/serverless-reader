776a94c8d3b4fc53b8b4d5bf651de19eda9aada3
cocozxu <73022042+cocozxu@users.noreply.github.com>
2021-06-26 23:59:24 +0800

    Update index.js

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -8,7 +8,7 @@ module.exports = async function (context, req) {
 
     // parse the body
     var parts = multipart.Parse(body, boundary);
-    var convertedResult = Buffer.from(parts[0].data).toString('base64');
+    var convertedResult = Buffer.from(parts[0].data).toString('base64').substring(1);
     
     
 
