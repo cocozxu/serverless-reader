d07615af399e348485c2e2e41ecc903dcc961d04
cocozxu <73022042+cocozxu@users.noreply.github.com>
2021-07-02 23:55:34 +0800

    Update index.js

--- song4u/index.js
+++ song4u/index.js
@@ -34,7 +34,7 @@ module.exports = async function (context, req) {
 }
 
 async function analyzeImage(img){
-    const subscriptionKey = process.env.['subscriptionkey'];
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
     const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
     let params = new URLSearchParams({
         'returnFaceId': 'true',
