d53d01c977bbf82dbdd294b609c58dd0bc57d7a6
cocozxu <zx35@cornell.edu>
2021-07-05 11:04:15 +0800

    as

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,13 +1,38 @@
+var multipart = require("parse-multipart")
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=createmystorageaccoun;AccountKey=Zho2VHb0+qhiL6mA6Rvg0/jWzOWtFT80EW5cfJIMouV5GBAAd0sWxFQhY+rHeYvNeipmTNUaJTjZ5mGfkqcSqg==;EndpointSuffix=core.windows.net";
+const { BlobServiceClient } = require("@azure/storage-blob");
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    var body = req.body;
+    var parsedBody = multipart.Parse(body, boundary);
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    var filetype = parsedBody[0].type;
+    if (filetype == "image/png") {
+    ext = "png";
+    } else if (filetype == "image/jpeg") {
+    ext = "jpeg";
+    } else if (filetype == "image/jpg") {
+    ext = "jpg"
+    } else {
+    username = "invalidimage"
+    ext = "";
+    }
 
+    var responseMessage = await uploadFile(parsedBody, ext);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+    body: "file saved"
     };
+
+   
+}
+async function uploadFile(parsedBody, ext){
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const containerName = 'image';
+    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
+
+    const blobName = 'test.' + ext;    // Create the container
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
+
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+
 }
