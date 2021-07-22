877a9b5f67e5e8cd4ff4909a023666f1285fa9f5
cocozxu <zx35@cornell.edu>
2021-07-22 09:17:31 +0800

    a

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,17 +1,9 @@
 function getImage(event){
     event.preventDefault();
-<<<<<<< HEAD
     if(document.getElementById('name').value!=null){
         $('#output').text("Thanks!")
     }
     else{
         alert("No name error.")
-=======
-    if(document.getElementById("username").value!=null){
-        $('#output').text("Thanks")
->>>>>>> a9de1892a7d475832ac0f41fe3faa62afd2559a1
-    }
-    else{
-        alert("No name")
     }
 }
