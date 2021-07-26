0d0510afe259ec67811521a212540f3c015c6875
cocozxu <zx35@cornell.edu>
2021-07-26 13:20:48 +0800

    as

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,101 +1,68 @@
-function getImage(event){
-<<<<<<< HEAD
-    event.preventDefault()
-
-    const myform = document.getElementById("myform");
-    let nameInput = document.getElementById("name")
-    let fileInput = document.getElementById("image")
-    const file = fileInput.files[0]; // fileInput is the file upload input element
-    // get image uploaded, save in payload
-    var payload = new FormData(myform);
-    console.log(payload)
-    payload.append("file", file);
-    $('#output').text("Thanks!")
-
-    console.log(payload)
-    if (document.getElementById("name").value != ''){
-        try{
-            let url = "https://hackerr.azurewebsites.net/api/bunnimage-upload"
-             console.log("Image was uploaded, making POST req to azure func");
-            // create request to azure function!
-            const resp = fetch(url, {
-                method: 'POST',
-                headers: {
-                    'codename': nameInput.value // was username
-                },
-                body: payload
-                })
-            $('#output').text("Your image has been stored successfully!")
-        } catch (err){
-            $('#output').text(err)
-        }
-    }
-    else{
-        alert("No name error.")
-    }
-=======
-    event.preventDefault()
-
-    const myform = document.getElementById("myform");
-    let nameInput = document.getElementById("name")
-    let fileInput = document.getElementById("image")
-    const file = fileInput.files[0]; // fileInput is the file upload input element
-    // get image uploaded, save in payload
-    var payload = new FormData(myform);
-    console.log(payload)
-    payload.append("file", file);
-    $('#output').text("Thanks!")
-
-    console.log(payload)
-    if (document.getElementById("name").value != ''){
-        try{
-            let url = "https://hackerr.azurewebsites.net/api/bunnimage-upload"
-             console.log("Image was uploaded, making POST req to azure func");
-            // create request to azure function!
-            const resp = fetch(url, {
-                method: 'POST',
-                headers: {
-                    'codename': nameInput.value // was username
-                },
-                body: payload
-                })
-            $('#output').text("Your image has been stored successfully!")
-        } catch (err){
-            $('#output').text(err)
-        }
-    }
-    else{
-        alert("No name error.")
-    }
->>>>>>> b28d9293c4c7a4e9851874cf48cbf43a65d4ff48
-}
-
-async function downloadImage() {
-        let username = document.getElementById("downloadusername").value
-        if (username != '') {
+async function getImage(event) {
+      event.preventDefault();
+      var myform = document.getElementById('myform');
+      //get image and filename uploaded by user via the form
+      let nameInput = document.getElementById('username');
+      let fileInput = document.getElementById('image');
+      let file = fileInput.files[0]; //get image data
     
-            try {
-                let url = "https://hackerr.azurewebsites.net/api/bunnimage-download"
-            console.log("Got file name, making GET request to download image")
-            
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'username': username
-                }
-            })
+      var payload = new FormData(myform);
+      console.log(payload);
+      payload.append('file', file);
+      $('#output').text('Thanks!');
     
-            let data = await response.json();
-            let imageUrl = data.downloadUri
+      if (document.getElementById('username').value != '') {
+        try {
+          let url = 'https://hackerr.azurewebsites.net/api/bunnimage-upload';
+          console.log('Image was uploaded, making POST request to Azure function');
+          // create request to Azure functions
+          const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+              "codename": nameInput.value
+            },
+            body: payload,
+          });
     
-            console.log("Made GET request successfully")
-    
-            window.open(imageUrl, "_self")
-            } catch (error) {
-                alert(error)
-            }
-            
-        }else{
-            alert("No name error.")
+          $('#output').text('Your image has been stored successfully!');
+        } catch (err) {
+          $('#output').text(err);
         }
+      } else {
+        alert('No name error.');
+      }
+    }
+    
+    async function downloadImage () {
+      let username = document.getElementById(('downloadusername').value)
+    
+      if ( username != '') {
+    
+       try {
+        let url = "https://hackerr.azurewebsites.net/api/bunnimage-download"
+    
+        console.log("Documented file name; making GET request to download image"); 
+      
+        const resp = await fetch (url, {
+          method: 'GET',
+          header: {
+            "username": username
+          }
+        })
+       
+          let data = await resp.json ()
+          let imageUrl = data.downloadUri
+      
+          console.log("Made GET request successfully");
+      
+          window.open(imageUrl, "_self")
+      
+       } catch(err) {
+          alert(err)
+       }
+    
+      
+      } else {
+        alert('No name error.');
+      }
     }
