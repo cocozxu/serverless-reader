ed6838b550ed7976c7cf3bc864b20b5827d848e6
cocozxu <73022042+cocozxu@users.noreply.github.com>
2021-07-26 12:12:50 +0800

    Update script.js

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,33 +1,35 @@
 function getImage(event){
-    event.preventDefault();
-    const bunniForm = document.getElementById("myform");
-    const name = document.getElementById("name");
-    const fileInput = document.getElementById("image");
-    const payload = new FormData(bunniForm);
-    const file = fileInput.files[0]; // fileInput is the file upload input element
-    payload.append("file", file);
+    event.preventDefault()
 
-    if(document.getElementById('name').value!=null){
-        try{
-            $('#output').text("Thanks!")
+    const myform = document.getElementById("myform");
+    let nameInput = document.getElementById("name")
+    let fileInput = document.getElementById("image")
+    const file = fileInput.files[0]; // fileInput is the file upload input element
+    // get image uploaded, save in payload
+    var payload = new FormData(myform);
+    console.log(payload)
+    payload.append("file", file);
+    $('#output').text("Thanks!")
 
-        const response = fetch("https://hackerr.azurewebsites.net/api/bunnimage-upload?", {
-            method: 'POST',
-            headers:{
-                'codename': username
-            },
-            body: payload
-        })
-        $('#output').text("Your image has been stored successfully!")
-    
-
-        }catch(err){
-            $('#output').text(err)
-        }
-    }
-    else{
-        alert("No name error.")
-    }
-   
-
+    console.log(payload)
+    if (document.getElementById("name").value != ''){
+        try{
+            let url = "https://hackerr.azurewebsites.net/api/bunnimage-upload"
+             console.log("Image was uploaded, making POST req to azure func");
+            // create request to azure function!
+            const resp = fetch(url, {
+                method: 'POST',
+                headers: {
+                    'codename': nameInput.value // was username
+                },
+                body: payload
+                })
+            $('#output').text("Your image has been stored successfully!")
+        } catch (err){
+            $('#output').text(err)
+        }
+    }
+    else{
+        alert("No name error.")
+    }
 }
