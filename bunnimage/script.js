6022281a90a7d0e5822ba7f173f3e0fe851695be
cocozxu <zx35@cornell.edu>
2021-07-26 13:10:59 +0800

    a

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,4 +1,5 @@
 function getImage(event){
+<<<<<<< HEAD
     event.preventDefault()
 
     const myform = document.getElementById("myform");
@@ -32,6 +33,41 @@ function getImage(event){
     else{
         alert("No name error.")
     }
+=======
+    event.preventDefault()
+
+    const myform = document.getElementById("myform");
+    let nameInput = document.getElementById("name")
+    let fileInput = document.getElementById("image")
+    const file = fileInput.files[0]; // fileInput is the file upload input element
+    // get image uploaded, save in payload
+    var payload = new FormData(myform);
+    console.log(payload)
+    payload.append("file", file);
+    $('#output').text("Thanks!")
+
+    console.log(payload)
+    if (document.getElementById("name").value != ''){
+        try{
+            let url = "https://hackerr.azurewebsites.net/api/bunnimage-upload"
+             console.log("Image was uploaded, making POST req to azure func");
+            // create request to azure function!
+            const resp = fetch(url, {
+                method: 'POST',
+                headers: {
+                    'codename': nameInput.value // was username
+                },
+                body: payload
+                })
+            $('#output').text("Your image has been stored successfully!")
+        } catch (err){
+            $('#output').text(err)
+        }
+    }
+    else{
+        alert("No name error.")
+    }
+>>>>>>> b28d9293c4c7a4e9851874cf48cbf43a65d4ff48
 }
 
 async function downloadImage() {
