fc19a4678ea70ad98452513f411253a56d155efa
cocozxu <zx35@cornell.edu>
2021-07-26 12:00:12 +0800

    a

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,9 +1,33 @@
 function getImage(event){
     event.preventDefault();
+    const bunniForm = document.getElementById("myform");
+    const name = document.getElementById("name");
+    const fileInput = document.getElementById("image");
+    const payload = new FormData(bunniForm);
+    const file = fileInput.files[0]; // fileInput is the file upload input element
+    payload.append("file", file);
+
     if(document.getElementById('name').value!=null){
-        $('#output').text("Thanks!")
+        try{
+            $('#output').text("Thanks!")
+
+        const response = await fetch("https://hackerr.azurewebsites.net/api/bunnimage-upload?", {
+            method: 'POST',
+            headers:{
+                'codename': username
+            },
+            body: payload
+        })
+        $('#output').text("Your image has been stored successfully!")
+    
+
+        }catch(err){
+            $('#output').text(err)
+        }
     }
     else{
         alert("No name error.")
     }
+   
+
 }
