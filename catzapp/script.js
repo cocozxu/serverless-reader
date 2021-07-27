a9bcb1f9adf56c5523d8143ea64127185d208f6d
cocozxu <73022042+cocozxu@users.noreply.github.com>
2021-07-27 11:35:01 +0800

    Update script.js

--- catzapp/script.js
+++ catzapp/script.js
@@ -2,21 +2,21 @@ function y1k3s() {
     let name1 = document.getElementById("name1").value
     let endpoint1 = "https://cataas.com/cat/says/" + name1
     if(name1 != '') {
-    document.getElementById("image1").src = endpoint1
+    document.getElementById("image1").src = “data:image/png;base64,”+endpoint1
     }
     let name2 = document.getElementById("name2").value
     let endpoint2 = "https://cataas.com/cat/says/" + name2
     if(name2 != '') {
-    document.getElementById("image2").src = endpoint2
+    document.getElementById("image2").src = “data:image/png;base64,”+endpoint2
     }
     let name3 = document.getElementById("name3").value
     let endpoint3 = "https://cataas.com/cat/says/" + name3
     if(name3 != '') {
-    document.getElementById("image3").src = endpoint3
+    document.getElementById("image3").src = “data:image/png;base64,”+endpoint3
     }
     let name4 = document.getElementById("name4").value
     let endpoint4 = "https://cataas.com/cat/says/" + name4
     if(name4 != '') {
-    document.getElementById("image4").src = endpoint4
+    document.getElementById("image4").src = “data:image/png;base64,”+endpoint4
     }
     }
