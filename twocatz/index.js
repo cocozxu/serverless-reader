b602bd89d02c0c69fd44edfc57aee31aad2dcd23
cocozxu <zx35@cornell.edu>
2021-07-27 10:01:10 +0800

    as

--- twocatz/index.js
+++ twocatz/index.js
@@ -6,34 +6,37 @@ module.exports = async function (context, req) {
     
     var names = ["Shreya","Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"];
     var random_value = Math.floor(names.length * Math.random());
-    var name1 = names[random_value];
+    var name1 = req.query.name1;
+    var name2 = req.query.name2;
+    var name3 = req.query.name3;
+    var name4 = req.query.name4;
 
-    var random_value2 = Math.floor(names.length * Math.random());
-    var name2 = names[random_value2];
-
-    let resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {
-        method: 'GET'
-    });
-
-    let data = await resp.arrayBuffer()
-
-    let base64data1 = Buffer.from(data).toString('base64')
-    // we need to receive it as a buffer since this is an image we are receiving from the API
-    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    
-    let resp1 = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {
+    async function getCat(name){
+        let endpoint="https://cataas.com/cat/cute/says/"+name
+        let resp1 = await fetch(endpoint, {
         method: 'GET'
     });
 
     let data1 = await resp1.arrayBuffer()
 
     let base64data2 = Buffer.from(data1).toString('base64')
+    return base64data2;
 
+    }
+    
+
+   let cat1=await getCat(name1)
+   let cat2=await getCat(name2)
+   let cat3=await getCat(name3)
+   let cat4=await getCat(name4)
+
+    
     context.res = {
         body: {
-            cat1: base64data1,
-            cat2: base64data2,
-            names: [name1, name2]
+            cat1: cat1,
+            cat2: cat2,
+            cat3: cat3,
+            cat4: cat4
         }
     };
 }
