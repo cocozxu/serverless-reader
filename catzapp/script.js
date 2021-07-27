b3ce43c5558d546d8fcb725238fc438788b5f815
cocozxu <73022042+cocozxu@users.noreply.github.com>
2021-07-27 09:44:34 +0800

    Update script.js

--- catzapp/script.js
+++ catzapp/script.js
@@ -1,6 +1,6 @@
 function y1k3s() {
     let name = document.getElementById("name").value
-    let endpoint = "https://cataas.com/cat/cute/says/" + name
+    let endpoint = "https://cataas.com/cat/says/" + name
     if(name != '') {
     document.getElementById("image").src = endpoint
     }
